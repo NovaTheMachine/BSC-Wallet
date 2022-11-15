6af5a80e0e5bec934c4505e48fdfa7a5a94035d0
NovaTheMachine <n.fdistri@gmail.com>
2022-11-15 13:34:58 +0100

    Get oza balance

--- BSC-wallet.js
+++ BSC-wallet.js
@@ -1,13 +1,21 @@
 const { ethers } = require("ethers");
+const genericErc20Abi = require("./ERC20.json");
+const tokenContractAddress = "0x1E16D4579D6a1471745a20eC491739e201971151";
+//console.log(genericErc20Abi)
 
 const provider = new ethers.providers.JsonRpcProvider(
   "https://bsc-dataseed4.binance.org"
 );
+const contract = new ethers.Contract(
+  tokenContractAddress,
+  genericErc20Abi,
+  provider
+);
 
 async function getBalances(address) {
-  let rawBalance = await provider.getBalance(address);
-  let Balance = ethers.utils.formatEther(rawBalance);
+  const rawBalance = await contract.balanceOf(address);
+  let Balance = ethers.utils.formatEther(rawBalance.toString());
   console.log(Balance);
 }
 
-getBalances("0xc790cccAe547d5B779e9DE94DCFc9C9ec12280c0");
+getBalances("0xe68a8780f369b24d94514290a535d9477ee4c68c");
